7a1ae2bb768015aa485b1b1297dd61e514b378cf
cz_dell <chengzi12130@gmail.com>
2018-02-01 23:00:15 +0800

    Add project files.

---
+++ ConsoleApp3/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Logger
+    {
+        public static void Error(string str)
+        {
+            //Console.WriteLine(str);
+            System.Diagnostics.Debug.WriteLine(str);
+        }
+
+        public static void Debug(string str)
+        {
+            //Console.WriteLine(str);
+            System.Diagnostics.Debug.WriteLine(str);
+        }
+    }
+}
